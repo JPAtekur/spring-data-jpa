d8391aa9919b5262f67fee4ddf4bbaca71c3713a
Atekur Rahman <jp7atik@gmail.com>
2021-11-26 23:47:29 +0600

    first commit

---
+++ src/test/java/com/practice/spring/data/jpa/SpringDataJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
